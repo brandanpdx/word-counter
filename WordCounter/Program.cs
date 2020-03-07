6b8930f14d6ff4710bef20e73c0c7d6ae1ac726c
Brandan Sayarath <brandan@brandan.tech>
2020-03-06 20:02:06 -0800

    add user interface

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,7 +7,21 @@ namespace WordCounter
   {
     public static void Main()
     {
-     Console.WriteLine();
+      Start();
+    }
+
+    public static void Start()
+    {
+      Console.WriteLine("Please enter a sentence:");
+      string usersentence = Console.ReadLine();
+      Console.WriteLine("Please enter a word to check:");
+      string userword = Console.ReadLine();
+      CountWord newCountWord = new CountWord(userword,usersentence);
+
+      int result = newCountWord.CountRepeat();
+
+      Console.WriteLine("The word " + userword + " appears " + result + " time(s) in the sentence.");
+
     }
   }
 
