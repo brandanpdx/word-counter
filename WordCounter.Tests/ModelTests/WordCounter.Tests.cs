b1c3a952c9a3093763bb003798bb4dbf053e383a
Brandan Sayarath <brandan@brandan.tech>
2020-03-08 22:53:25 -0700

    test if CountWord takes in single letter and fail it meaningfully

--- WordCounter.Tests/ModelTests/WordCounter.Tests.cs
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -10,18 +10,19 @@ namespace WordCounter.Tests
     [TestMethod]
     public void CountWord_Instantiates_NewInstance()
     {
-      // Arrange
       CountWord newCountWord = new CountWord("test", "this is only a test");
-      // Assert
       Assert.AreEqual(typeof(CountWord), newCountWord.GetType());
     }
-
+    [TestMethod]
+    public void CountWord_TakesInOneCharacter_A()
+    {
+      CountWord newCountWord = new CountWord("a", "");
+      Assert.AreEqual("b", newCountWord.UserWord);
+    }
     [TestMethod]
     public void CountRepeat_CountsWordsInSentence_1()
     {
-      // Arrange
       CountWord newCountWord = new CountWord("eggs", "eggs are one of my favorite breakfast food yay eggs");    
-      // Assert
       Assert.AreEqual(2, newCountWord.CountRepeat());
     }
   }
